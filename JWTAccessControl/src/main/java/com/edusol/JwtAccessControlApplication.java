992374c1de244c5900b8590a492bc9aca4794e70
amit07bti <amitkumar.developer764@gmail.com>
2025-07-17 09:25:42 +0530

    bassic crud API implemented

---
+++ JWTAccessControl/src/main/java/com/edusol/JwtAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.edusol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAccessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAccessControlApplication.class, args);
+	}
+
+}
